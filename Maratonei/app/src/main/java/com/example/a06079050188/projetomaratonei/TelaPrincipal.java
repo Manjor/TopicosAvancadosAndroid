10f0a6384be8ab5a6eea94d1722e9054f0fb9f93
Manoel Tavares <36523015+Manjor@users.noreply.github.com>
2018-08-17 14:01:36 -0300

    Alteracao de TabLayout
    TabLayout Adiconado

--- Maratonei/app/src/main/java/com/example/a06079050188/projetomaratonei/TelaPrincipal.java
+++ Maratonei/app/src/main/java/com/example/a06079050188/projetomaratonei/TelaPrincipal.java
@@ -1,5 +1,7 @@
 package com.example.a06079050188.projetomaratonei;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,18 @@ public class TelaPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+
+
+        AbasInicio adaptar = new AbasInicio(getSupportFragmentManager());
+
+        adaptar.adicionar(new MinhaLista() , "Primeira Aba");
+        adaptar.adicionar(new SegundoFragment() , "Segunda Aba");
+        adaptar.adicionar(new TerceiroFragment() , "Terceira Aba");
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.abas_view_pager);
+        viewPager.setAdapter(adaptar);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.abas);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
